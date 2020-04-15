664ab0642f7d2a37471ee63569e4ac2779b84669
Francis Bukenya <frankie.bukenya@gmail.com>
2020-04-15 02:04:19 +0200

    Add brain left and right

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './Home.scss';
 import Default from '../layouts/Default';
 
 class Home extends Component {
@@ -6,7 +7,19 @@ class Home extends Component {
     return (
       <Default>
         <div className="Home">
-          <h1 className="Home-title">Mentally.me</h1>
+          <h1 className="home-title">Mentally.me</h1>
+          <div className="home-img-container">
+            <img
+              src="https://res.cloudinary.com/frankie-dev/image/upload/v1586406741/images/leftBrain.svg"
+              alt=""
+              className="home-img brain-left"
+            />
+            <img
+              src="https://res.cloudinary.com/frankie-dev/image/upload/v1586406741/images/rightBrain.svg"
+              alt=""
+              className="home-img brain-right"
+            />
+          </div>
         </div>
       </Default>
     );
