a0187468ff245f41493025de6d7ce62c1a867382
Francis Bukenya <frankie.bukenya@gmail.com>
2020-04-16 07:53:58 +0200

    Modify text container

--- src/components/TextContainer/TextContainer.jsx
+++ src/components/TextContainer/TextContainer.jsx
@@ -5,15 +5,19 @@ import './TextContainer.css';
 const TextContainer = ({ users }) => (
   <div className="textContainer">
     <div>
-      <h1>Mentally Me</h1>
-      <h2>A simple chat app in time of quarantine</h2>
-      <h2>Just because we are isolated does not mean you we are alone</h2>
+      <h1 className="text-container-title">This is a safe space</h1>
+      <h2 className="text-container-subheading">
+        A compassion oriented chat app
+      </h2>
+      <h3 className="text-container-tagline">
+        Just because we are isolated does not mean you we are alone
+      </h3>
     </div>
     {users ? (
       <div>
-        <h1>People currently chatting:</h1>
+        <h3>People currently chatting:</h3>
         <div className="activeContainer">
-          <h2>
+          <h2 className="text-container-subheading">
             {users.map(({ name }) => (
               <div key={name} className="activeItem">
                 {name}
