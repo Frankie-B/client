34c154caf8a0154d4d8dba5c6c448f70c2050f43
Francis Bukenya <frankie.bukenya@gmail.com>
2020-04-27 14:23:05 +0200

    Add new buttons to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,13 +10,27 @@ class Home extends Component {
         <div className="Home">
           <h1 className="home-title">Mentally.me</h1>
           <div className="home-mobile-btns">
+            <div className="home-img-container">
+              <img
+                src="https://res.cloudinary.com/frankie-dev/image/upload/v1586406741/images/leftBrain.svg"
+                alt=""
+                className="home-img brain-left"
+              />
+              <img
+                src="https://res.cloudinary.com/frankie-dev/image/upload/v1586406741/images/rightBrain.svg"
+                alt=""
+                className="home-img brain-right"
+              />
+            </div>
             <Link to="/signup" className="btn-left">
-              <button className="btn btn-primary btn-md d-lg-none">
+              <button className={'home-btn btn-mobile signup mt-20 d-lg-none'}>
                 Sign Up
               </button>
             </Link>
             <Link to="/login" className="btn-right">
-              <button className="btn btn-light btn-md d-lg-none">Log In</button>
+              <button className={'home-btn btn-mobile login mt-20 d-lg-none'}>
+                Log In
+              </button>
             </Link>
           </div>
         </div>
